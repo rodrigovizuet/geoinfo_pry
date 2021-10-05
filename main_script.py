205f2d007bc14d59db903d1a7fb84318107c79ec
rodrigovizuet <lgonviz@hotmail.com>
2021-10-05 18:31:58 -0500

    codigo listo para segmentar ciclovias y extraer centroides

--- main_script.py
+++ main_script.py
@@ -1,5 +1,82 @@
 '''
-Codigo de python
+####################################
+## Accesibilidad a taller de bici ##
+####################################
 '''
+#%% Paquetería
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import *
+from access import Access, weights
+import matplotlib.pyplot as plt
+import contextily as ctx
+import os
 
-# Paquetería
+os.chdir('/home/lvizuet/Git/geoinfo_pry/')
+
+# Funciones
+def multi_line(line, dista):
+    if line.geom_type == 'MultiLineString':
+        segments = []
+        for l in line:
+            segments += break_line(l, dista)
+    else:
+        segments = break_line(line, dista)
+    return segments
+
+def break_line(line, dist):
+    line = LineString([xy[0:2] for xy in list(line.coords)])
+    if line.length <= dist:
+        return [line]
+    else: 
+        segments = cut(line, dist)
+        return [segments[0]] + break_line(segments[1], dist)
+
+def cut(line, distance):
+    if distance <= 0.0 or distance >= line.length:
+        return [line]
+    coords = list(line.coords)
+    for i, p in enumerate(coords):
+        p = line.project(Point(p)) 
+        if p == distance:
+            return [
+                LineString(coords[:i+1]),
+                LineString(coords[i:])]
+        if p > distance:
+            cp = line.interpolate(distance)
+            return [
+                LineString(coords[:i] + [(cp.x, cp.y)]),
+                LineString([(cp.x, cp.y)] + coords[i:])]
+
+#%% Ciclovias
+ciclo = gpd.read_file('data/ciclovias_cdmx.zip')
+ciclo_lines = pd.DataFrame({'ID':[], 'NOMBRE':[], 'TIPO_IC':[], 
+                            'VIALIDAD':[], 'TIPO_VIA':[], 'ESTADO':[], 
+                            'SENTIDO':[], 'INSTANCIA':[], 'AÑO':[],'geometry':[]})
+for i in range(ciclo.shape[0]):
+    df_l = pd.DataFrame({'ID':ciclo.iloc[i,0], 'NOMBRE':ciclo.iloc[i,1],
+                         'TIPO_IC':ciclo.iloc[i,2], 
+                         'VIALIDAD':ciclo.iloc[i,3],
+                         'TIPO_VIA':ciclo.iloc[i,4], 
+                         'ESTADO':ciclo.iloc[i,5], 
+                         'SENTIDO':ciclo.iloc[i,6],
+                         'INSTANCIA':ciclo.iloc[i,7], 
+                         'AÑO':ciclo.iloc[i,8],
+                         'geometry':(multi_line(ciclo.iloc[i,-1], 1000))})
+    ciclo_lines = ciclo_lines.append(df_l)
+    
+ciclo_lines = gpd.GeoDataFrame(ciclo_lines)
+ciclo_lines.reset_index(drop=True, inplace=True)
+ciclo_lines.reset_index(inplace=True)
+ciclo_lines.columns = [x.lower() for x in ciclo_lines.columns]
+ciclo_lines.rename(columns={'index':'fid', 'id':'fid_org', 'año':'year'}, 
+                   inplace=True)
+ciclo_centroids = ciclo_lines.copy()
+ciclo_centroids.loc[:,'geometry'] = ciclo_centroids.geometry.centroid
+
+#Exportar gpd's
+ciclo_lines.to_file('products/ciclovias_lines.gpkg', driver='GPKG', layer='ciclovias')
+ciclo_centroids.to_file('products/ciclovias_centroids.gpkg', driver='GPKG', layer='ciclovias_ctr')
+del ciclo, df_l, i
+
+
